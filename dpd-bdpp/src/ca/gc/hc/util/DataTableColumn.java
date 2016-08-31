9147a725c7bcf95557249925d0bfff5970cb17d5
sylarivi <sylvain.lariviere@canada.ca>
2016-08-31 16:26:33 -0400

    Implement DataTable server-side processing
    New class represents a DataTable column

---
+++ dpd-bdpp/src/ca/gc/hc/util/DataTableColumn.java
@@ -0,0 +1,59 @@
+package ca.gc.hc.util;
+
+public class DataTableColumn {
+	private int columnIndex = 0;
+	private String name = "";
+	private String fieldName = "";
+	private boolean isSearchable = false;
+	private boolean isSortable = false;
+	private String sortDirection = null;
+
+	public int getColumnIndex() {
+		return columnIndex;
+	}
+
+	public void setColumnIndex(int columnIndex) {
+		this.columnIndex = columnIndex;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isSearchable() {
+		return isSearchable;
+	}
+
+	public void setSearchable(boolean isSearchable) {
+		this.isSearchable = isSearchable;
+	}
+
+	public boolean isSortable() {
+		return isSortable;
+	}
+
+	public void setSortable(boolean isSortable) {
+		this.isSortable = isSortable;
+	}
+
+	public String getSortDirection() {
+		return sortDirection;
+	}
+
+	public void setSortDirection(String sortDirection) {
+		this.sortDirection = sortDirection;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+}
