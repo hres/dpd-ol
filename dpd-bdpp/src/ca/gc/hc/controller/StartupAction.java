55eab2b807e176fe85711bf42f56cebd4b26c461
sylarivi <sylvain.lariviere@canada.ca>
2016-09-08 10:48:38 -0400

    Organized imports

--- dpd-bdpp/src/ca/gc/hc/controller/StartupAction.java
+++ dpd-bdpp/src/ca/gc/hc/controller/StartupAction.java
@@ -11,7 +11,6 @@ import goc.webtemplate.component.jsp.MasterApplicationBean;
 import goc.webtemplate.jsp.beans.ApplicationBaseSettingsBean;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.ServletException;
